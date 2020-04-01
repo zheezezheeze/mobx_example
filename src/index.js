a32b57e860afcc2759716fab06396a9e7eb98a03
박지은 <zheeze@theopusone.com>
2020-04-01 15:13:13 +0900

    두번째

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import serviceWorker from './serviceWorker';
+import CounterStore from './stores/counter';
+
+const counter = new CounterStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider counter={counter}>
+    {/*  Provider에 props로 넣어준다.*/}
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker();
