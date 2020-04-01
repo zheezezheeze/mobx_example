aa5e7e04f446f7442c5b96bfa0c645bb15735f3d
박지은 <zheeze@theopusone.com>
2020-04-01 15:19:09 +0900

    세번째

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 
-@inject('counter')
+@inject( stores => ({
+    number: stores.counter.number,
+    increase: stores.counter.increase,
+    decrease: stores.counter.decrease
+}))
 @observer
 class Counter extends Component {
     render() {
-        const { counter } = this.props;
+        const { number, increase, decrease } = this.props;
         return (
             <div>
-                <h1>{counter.number}</h1>
-                <button onClick={counter.increase}>+1</button>
-                <button onClick={counter.decrease}>-1</button>
+                <h1>{number}</h1>
+                <button onClick={increase}>+1</button>
+                <button onClick={decrease}>-1</button>
             </div>
         );
     }
